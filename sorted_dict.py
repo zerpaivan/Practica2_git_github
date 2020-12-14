a5e9252cad7ccae82e7ce9b12a7b3b8b8964396d
Ivan Zerpa <zerpaivan@gmail.com>
2020-12-14 00:17:12 -0400

    add sorted funtion dict

--- sorted_dict.py
+++ sorted_dict.py
@@ -1,5 +1,10 @@
-def sorted_dict(dict):
+def sortedDict(dict):
     sorted_dict = {}
-    sorted_dict.fromkeys(sorted(list(dict.keys()), 0)
+    sorted_dict = sorted_dict.fromkeys(sorted(dict.keys()))
+    for i in sorted_dict:
+        sorted_dict[i] = sorted(dict[i])
+    return sorted_dict
 
-    
+if __name__ == "__main__":
+    d1 = {"x":[3,2,1], "a": [-7, 5, -1], "j": [3], "d": [4,0]}
+    print(sortedDict(d1))
