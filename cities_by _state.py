cf1e6f3ab25dde53ae8751b2909f29ae3fec27aa
Ivan Zerpa <zerpaivan@gmail.com>
2020-12-13 22:49:49 -0400

    add loop for group cities by states

--- cities_by _state.py
+++ cities_by _state.py
@@ -1,8 +1,23 @@
 file = open("cities_and_states.txt", "r")
 list_cities_states = file.readlines()
 file.close()
-
+print(list_cities_states)
 # bucle que elimina los espacios en blanco al final y al inicio
 # de cada elemento de la lista
-for i in len(list_cities_states):
+for i in range(len(list_cities_states)):
     list_cities_states[i] = list_cities_states[i].strip()
+
+
+#blucle que crea lista de los estados
+states_plus_cities = {}
+for n, c_s in enumerate(list_cities_states, 1):
+    temp = c_s.split(" - ")
+
+
+    if temp[1] not in states_plus_cities:
+        states_plus_cities[temp[1]] = []
+        states_plus_cities[temp[1]].append(temp[0])
+    else:
+        states_plus_cities[temp[1]].append(temp[0])
+
+print(states_plus_cities)
